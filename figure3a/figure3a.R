d059156b0233aebaec9cbe15cbefb357f1eee87c
Keith Lumbard <klumbard@gmail.com>
2024-03-15 13:07:34 -0400

    Updated Figure 3A.

--- figure3a/figure3a.R
+++ figure3a/figure3a.R
@@ -1,71 +1,113 @@
+library(readxl)
 library(tidyverse)
-library(ComplexHeatmap)
-library(circlize)
 library(recipes)
+library(circlize)
+library(ComplexHeatmap)
+library(stringr)
 
-dat.ids <- read_csv("training_ids.csv") %>% select(id, USUBJID)
-dat.ct <- read_csv("dat_ct.csv")
-dat.ct.noncancer <- dat.ct %>% filter(status == 0)
-dat.cv <- read_csv("dat_cv.csv")
+dat.scores <- 
+  read_csv("./data/prediction_lucas.csv") %>% select(id, `Delfi Score` = score.seq)
 
 dat.meta <- 
-  read_csv("./combined_ct_cv_metadata.csv") %>% 
-  inner_join(dat.ids) %>% 
-  select(id, Stage = LCNSTAGE, Age = AGE, Sex = SEX, `Pack Years` = SUPACKYR, `Smoking Status` = SUCAT, Histology=LCNHIST) %>%
+  read_excel("./data/LUCAS_metadata.xlsx", sheet = 1) %>% 
+  filter(id %in% dat.scores$id) %>%
+  rename(status = type, stage = Stage)
+
+dat.labels <- 
+  dat.meta %>% 
+  select(id, status, stage) 
+
+dat.features.raw <- 
+  read_csv("./data/training-set.csv") 
+
+dat.full <- 
+  dat.features.raw %>% 
+  inner_join(dat.labels) %>% 
+  select(id, status, stage, 
+         starts_with("ratio_"), starts_with("zscore_"), 
+         Age = clinical_age, `Smoking Status` = clinical_smokingstatus, `Pack Years` = clinical_packyears) %>%
   mutate(`Pack Years` = case_when(`Pack Years` <= 30 ~ "30 or fewer",
                                   `Pack Years` >= 31 & `Pack Years` <= 60 ~ "31 - 60",
                                   `Pack Years` >= 61 & `Pack Years` <= 90 ~ "61 - 90",
                                   `Pack Years` >= 91 & `Pack Years` <= 120 ~ "91 - 120",
-                                  `Pack Years` >= 121 ~ "121 or more"),
-         Histology = case_when(Histology == "Adenocarcinoma" ~ "Adenocarcinoma",
-                               Histology == "Squamous cell carcinoma" ~ "Squamous cell carcinoma",
-                               Histology == "Not Confirmed Lung Cancer" ~ "Non-cancer",
-                               TRUE ~ "Other")) %>%
-  mutate(`Pack Years` = factor(`Pack Years`, levels = rev(c("30 or fewer", "31 - 60", "61 - 90", "91 - 120", "121 or more"))))
+                                  `Pack Years` >= 121 ~ "121 or more")) %>% 
+  mutate(`Pack Years` = factor(`Pack Years`, levels = rev(c("30 or fewer", "31 - 60", "61 - 90", "91 - 120", "121 or more")))) %>% 
+  mutate(`Smoking Status` = str_to_title(`Smoking Status`)) %>% 
+  mutate(`Smoking Status` = factor(`Smoking Status`, levels = c("Never", "Former", "Current")))
 
-scores <- 
-  read_csv("final_scores.csv") %>% 
-  select(id, `Delfi Score` = score) %>%
-  inner_join(dat.meta)
+dat.features.all <- 
+  dat.full %>% 
+  select(id, status, starts_with("ratio_"), starts_with("zscore_"))
 
-recipe.ct.noncancer <- 
-  recipe(status ~ ., data = dat.ct.noncancer) %>%
+dat.features.noncancer <- 
+  dat.full %>% 
+  filter(status == "healthy") %>% 
+  select(id, status, starts_with("ratio_"), starts_with("zscore_"))
+
+dat.features.noncancer <- 
+  dat.full %>% 
+  filter(status == "healthy") %>% 
+  select(id, status, starts_with("ratio_"), starts_with("zscore_"))
+
+dat.clinical <- 
+  dat.full %>% 
+  select(id, Age, `Smoking Status`, `Pack Years`)
+
+recipe.noncancer <- 
+  recipe(status ~ ., data = dat.features.noncancer) %>%
   update_role(id, new_role="ID") %>%
   step_normalize(all_predictors()) %>%
   prep()
 
-baked.ct <-
-  recipe.ct.noncancer %>%
-  bake(new_data = dat.ct) %>%
-  mutate(set = "CT") %>%
-  mutate(id = dat.ct$id) %>%
-  inner_join(scores) %>%
+baked.full <-
+  recipe.noncancer %>%
+  bake(new_data = dat.features.all) %>%
+  mutate(id = dat.features.all$id) %>%
+  mutate(status = dat.features.all$status) %>% 
+  inner_join(dat.scores) %>%
   arrange(desc(`Delfi Score`)) %>%
-  mutate(Status = if_else(status == 1, "Lung Cancer", "Non-cancer")) %>%
-  mutate(Status = factor(Status, levels = c("Non-cancer", "Lung Cancer"))) %>%
-  mutate(Stage = if_else(Stage == "Not Confirmed Lung Cancer", "Non-cancer", Stage)) 
+  inner_join(dat.meta) %>% 
+  mutate(Status = if_else(status == "cancer", "Lung Cancer", "Non-cancer")) %>%
+  mutate(Status = factor(Status, levels = c("Non-cancer", "Lung Cancer"))) %>% 
+  mutate(Stage = case_when(stage %in% c("IA", "IB") ~ "I",
+                           stage %in% c("IIA", "IIB") ~ "II",
+                           stage %in% c("IIIA", "IIIB", "IIIC") ~ "III",
+                           stage %in% c("IV") ~ "IV",
+                           TRUE ~ "Non-cancer")) %>% 
+  inner_join(dat.clinical)
 
-baked.ct.heatmap <- 
-  baked.ct %>% 
-  select(contains(c("centeredslratio", "zscore", "mxab", "mtDNA", "mixture"))) %>% 
+baked.heatmap <- 
+  baked.full %>% 
+  select(starts_with("ratio_"), starts_with("zscore_")) %>% 
   as.matrix()
 
-annot.rows.ct <- 
-  baked.ct[, c('Delfi Score', 'Status', 'Stage', 'Histology', 'Age', 'Sex', 'Smoking Status', 'Pack Years')]
+annot.rows <- 
+  baked.full[, c('Delfi Score', 'Status', 'Stage', 'Age', 'Smoking Status', 'Pack Years')]
 
-col.fun.row = list(Age = colorRamp2(c(50, 90), c('#f7fcf0', '#4eb3d3')) ,
-                   Sex = structure(c('#807dba', '#dadaeb'), names = c('Male', 'Female')),
-                   Histology = structure(c('#faebd7', '#ee82ee', '#483d8b', '#add8e6'), names=c('Non-cancer', 'Adenocarcinoma', 'Squamous cell carcinoma', 'Other')),
-                   `Smoking Status` = structure(c('#ffffff', '#737373'), names = c("Former", "Current")),
+col.fun.row = list(Age = colorRamp2(c(23, 94), c('#f7fcf0', '#4eb3d3')) ,
+                   `Smoking Status` = structure(c('#ffffff', '#737373', 'grey20'), names = c("Never", "Former", "Current")),
                    `Pack Years` = structure(colorRampPalette(c("#f6eff7", "#3690c0"))(5), names = c("30 or fewer", "31 - 60", "61 - 90", "91 - 120", "121 or more")),
                    Status = structure(c('#f7f7f7', '#238b45'), names = c("Non-cancer", "Lung Cancer")),
                    Stage = structure(c('#eeeeee', '#fee5d9', '#fcae91', '#fb6a4a', '#cb181d'), names = c('Non-cancer', 'I', 'II','III','IV')),
                    `Delfi Score` = colorRamp2(c(0, 1), c('#ffffff', '#111111')))
-col.fun.column = list(Family = structure(c('#66c2a5', '#8da0cb', "#D55E00", "#CC79A7"), names = c('Chromosomal changes', 'Fragmentation profile', "mtDNA", "Length distribution")))
+
+annot.cols <-
+  data.frame(Family = sapply(colnames(baked.heatmap),
+                             function(x) {
+                               
+                               case_when(str_detect(x, "ratio_") ~ "Fragmentation profile",
+                                         str_detect(x, "zscore_") ~ "Chromosomal changes")
+                               
+                             })) %>%
+  mutate(Family = factor(Family, levels = c("Fragmentation profile", "Chromosomal changes")))
+
+names(annot.cols) <- "Feature Classes"
+col.fun.column = list(`Feature Classes` = structure(c("#8da0cb", "#66c2a5"), names = c("Fragmentation profile", "Chromosomal changes")))
+
 col.fun.body = colorRamp2(c(-2, -1, 0, 1, 2), rev(c('#d7191c', '#fdae61', '#ffffbf', '#abd9e9', '#2c7bb6')))
 
 ht.raw <- 
-  Heatmap(matrix = baked.ct.heatmap, 
+  Heatmap(matrix = baked.heatmap, 
           col = col.fun.body,
           name = "Values", 
           row_title = NULL,
@@ -74,7 +116,7 @@ ht.raw <-
           show_column_dend = FALSE,
           show_row_names = FALSE, 
           show_column_names = FALSE,
-          left_annotation = rowAnnotation(df = annot.rows.ct, 
+          left_annotation = rowAnnotation(df = annot.rows, 
                                           col = col.fun.row, 
                                           annotation_name_side= 'top', 
                                           annotation_name_offset = unit(3, "mm"),
@@ -92,7 +134,7 @@ ht.raw <-
                                       title_gp = gpar(fontsize = 7,
                                                       fontface = "bold")))
 
-ht <- 
+ht.out <- 
   columnAnnotation(df = annot.cols, 
                    col = col.fun.column, 
                    annotation_name_side = 'right',
@@ -102,6 +144,5 @@ ht <-
                                                                   fontface = "bold"))) %v% 
   ht.raw
 
-draw(ht, merge_legends = TRUE, padding = unit(c(1, 1, 27, 1), "mm"))
+draw(ht.out, merge_legends = TRUE, padding = unit(c(1, 1, 27, 1), "mm"))
 pdf("figure3a.pdf", height=6.69291, width=7.08661)
-
